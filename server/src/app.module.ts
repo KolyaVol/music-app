0c1ce661221923bd24759807e6fba7a114f01d88
VolNa <grizli887@mail.ru>
2024-06-02 11:05:45 +0300

    feat: add nesessary packages

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './configuration';
+import { TrackModule } from './track/track.module';
+import { FileModule } from './file/file.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TrackModule,
+    FileModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_DATA'),
+      }),
+    }),
+
+    // MongooseModule.forFeature([{ name: LogMongo.name, schema: LogSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
