a4d1b64adcfa581754698293afe48242e65fd68a
VolNa <grizli887@mail.ru>
2024-06-02 20:14:32 +0300

    feat: add main components

---
+++ client/src/components/TrackItem.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import { ITrack } from "../types/track";
+import { Card, Grid, IconButton } from "@mui/material";
+import styles from "../styles/TrackItem.module.scss";
+import { Delete, Pause, PlayArrow } from "@mui/icons-material";
+import { useRouter } from "next/router";
+import { useActions } from "../hooks/useActions";
+
+interface TrackItemProps {
+  track: ITrack;
+  active?: boolean;
+}
+
+const TrackItem: React.FC<TrackItemProps> = ({ track, active = false }) => {
+  const router = useRouter();
+  const { playTrack, pauseTrack, setActiveTrack } = useActions();
+
+  const play = (e: { stopPropagation: () => void }) => {
+    e.stopPropagation();
+    setActiveTrack(track);
+    playTrack();
+  };
+
+  return (
+    <Card
+      className={styles.track}
+      onClick={() => router.push("/tracks/" + track._id)}
+    >
+      <IconButton onClick={play}>
+        {!active ? <PlayArrow /> : <Pause />}
+      </IconButton>
+      <img
+        width={70}
+        height={70}
+        src={"http://localhost:5000/" + track.picture}
+      />
+      <Grid
+        container
+        direction="column"
+        style={{ width: 200, margin: "0 20px" }}
+      >
+        <div>{track.name}</div>
+        <div style={{ fontSize: 12, color: "gray" }}>{track.artist}</div>
+      </Grid>
+      {active && <div>02:42 / 03:22</div>}
+      <IconButton
+        onClick={(e) => e.stopPropagation()}
+        style={{ marginLeft: "auto" }}
+      >
+        <Delete />
+      </IconButton>
+    </Card>
+  );
+};
+
+export default TrackItem;
